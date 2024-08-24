db7e1b13da7a4317c50ebfc8c8d670338214f570
ian60509 <ian.60509@gmail.com>
2024-08-24 09:45:44 +0000

    :tada: read config & logger initialized

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,51 @@
 package main
 
 import (
-	"go-cleanarch/internal/router"
-	"log/slog"
+	"fmt"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	DB_DSN string `mapstructure:"DB_DSN"`
+	Username string `mapstructure:"Username"`
+	Password string `mapstructure:"Password"`
+	Network string `mapstructure:"Network"`
+	Server string `mapstructure:"Server"`
+	Port int `mapstructure:"Port"`
+	Name string `mapstructure:"Name"`
+}
+
+func initLogger() *zap.Logger {
+	logger, _ := zap.Config{
+		Encoding:         "console",
+		Level:            zap.NewAtomicLevelAt(zap.DebugLevel),
+		OutputPaths:      []string{"stdout"},
+		EncoderConfig: zapcore.EncoderConfig{
+				MessageKey:   "message",
+				LevelKey:     "level",
+				TimeKey:      "time",
+				NameKey:      "logger",
+				CallerKey:    "caller",
+				EncodeTime:    zapcore.ISO8601TimeEncoder,
+				EncodeLevel:   zapcore.LowercaseColorLevelEncoder, // can define by any self-defined function
+				EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}.Build()
+	
+	defer logger.Sync()
+
+	logger.Info("Logger initialized")
+	return logger
 }
 
 func readConfig(config *Config, path string) {
+	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
-	viper.SetDefault("DB_DSN", "")
+	viper.AddConfigPath("../config")
+	viper.SetDefault("DB_DSN", "") // Default value for DB_DSN
 	viper.AutomaticEnv()
 
 	if path != "" {
@@ -23,18 +55,31 @@ func readConfig(config *Config, path string) {
 	err := viper.ReadInConfig()
 	if err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			slog.Info("[Config] Config file not found. Reading from environment variables.")
+			fmt.Println("[Config] Config file not found. Reading from environment variables.")
 		} else {
-			slog.Error("[Config] Error reading config file", "err", err)
+			fmt.Println("[Config] Error reading config file", "err", err)
 			panic(err)
 		}
 	}
 
 	viper.Unmarshal(config)
+	fmt.Printf("[Config] Config read config: %+v\n", config)
 }
 
 func main() {
 	readConfig(&Config{}, "")
+	logger := initLogger()
 
-	router.NewRouter().Run(":8080")
+	//initialize database
+
+	//initialize service
+
+	//initialize repository
+	
+	//initialize contoller
+
+	//initialize router
+
+	logger.Info("Starting application")
+	// router.NewRouter().Run(":8080")
 }
