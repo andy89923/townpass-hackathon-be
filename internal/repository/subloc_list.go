f1ef8d77b4b5178b0cfeed635c6ec552d2e85be9
ming-hsien <w2bc8860@gmail.com>
2024-09-07 10:05:04 +0000

    feat: add subloc and loc list structure

---
+++ internal/repository/subloc_list.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	// "errors"
+	"go-cleanarch/pkg/domain"
+
+	"gorm.io/gorm"
+
+	"go.uber.org/zap"
+)
+
+type SubLocList struct {
+	templeId 		int 	`gorm:"column:temple_id"`
+	templeName 		string  `gorm:"column:temple_name"`
+	subTempleId   	string 	`gorm:"column:sub_temple_id"`
+	deity         	string  `gorm:"column:deity"`
+	description 	string	`gorm:"column:description"`
+}
+
+type postgresSubLocListRepository struct {
+	db *gorm.DB
+	logger *zap.Logger
+}
+
+
+func NewPostgresSubLocListRepository(db *gorm.DB, logger *zap.Logger) domain.LocationRepository {
+	return &postgresSubLocListRepository{
+		db: db,
+		logger: logger,
+	}
+}
--- internal/repository/visit_log.go
+++ internal/repository/visit_log.go
@@ -12,22 +12,34 @@ import (
 type VisitLog struct {
 	gorm.Model
 
-	UserId int
-	LocationId int
+	UserId        int
+	LocationId    int
 	SubLocationId int
 }
 
 type postgresVisitLogRepository struct {
-	db *gorm.DB
+	db     *gorm.DB
 	logger *zap.Logger
 }
 
-func (p *postgresVisitLogRepository ) AddVisitLog(visitLog domain.VisitLog) error {
+func (p *postgresVisitLogRepository) AddVisitLog(visitLog domain.VisitLog) error {
 	//TODO
 	return nil
 }
 
-func (p* postgresVisitLogRepository ) IsEventExist(userId int, locationId int) (bool, error) {
+func (p *postgresVisitLogRepository) IsEventExist(userId int, locationId int) (bool, error) {
 	//TODO
 	return false, nil
+}
+
+func (r *postgresVisitLogRepository) GetVisitedSubLocIdsByUserLocInfo(userId int, locationId int) []int {
+	var visitLogList []VisitLog
+	var visitedSubLocIds []int
+	r.db.Find(&visitLogList)
+	for _, visitLog := range visitLogList {
+		if visitLog.UserId == userId && visitLog.LocationId == locationId {
+			visitedSubLocIds = append(visitedSubLocIds, visitLog.SubLocationId)
+		}
+	}
+	return visitedSubLocIds
 }
