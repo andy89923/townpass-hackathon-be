dbe3f3ff674fdb684886bef8d7041c8e850c9ec7
HH <chh.cs12@nycu.edu.tw>
2024-09-07 11:45:09 +0000

    style: change name to Location

--- internal/router/Location.go
+++ internal/router/Location.go
@@ -1,12 +1,13 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
 	"go-cleanarch/internal/controller"
+
+	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
-func addBadgeGroup(router *gin.Engine, logger *zap.Logger, badgeController *controller.BadgeController) {
+func addBadgeGroup(router *gin.Engine, logger *zap.Logger, badgeController *controller.LocationController) {
 	BadgeGroup := router.Group("/api/v1/beacon")
 	{
 		BadgeGroup.GET("", badgeController.GetBadge)
