fafaa01c9bccceee7c59e0b91d504c22a7bd6e55
HH <chh.cs12@nycu.edu.tw>
2024-09-07 16:01:09 +0000

    feat: add tbMapRepository

--- internal/service/Location.go
+++ internal/service/Location.go
@@ -11,21 +11,24 @@ type LocationService struct {
 	locationRepository   domain.LocationRepository
 	locListRepository    domain.LocListRepository
 	subLocListRepository domain.SubLocListRepository
-	// visitLogRepository   domain.VisitLogRepository
-	logger *zap.Logger
+	visitLogRepository   domain.VisitLogRepository
+	tbMapRepository      domain.TbMapRepository
+	logger               *zap.Logger
 }
 
 func NewBadgeService(locationRepository domain.LocationRepository,
 	locListRepository domain.LocListRepository,
 	subLocListRepository domain.SubLocListRepository,
-	// visitLogRepository domain.VisitLogRepository,
+	visitLogRepository domain.VisitLogRepository,
+	tbMapRepository domain.TbMapRepository,
 	logger *zap.Logger) *LocationService {
 	return &LocationService{
 		locationRepository:   locationRepository,
 		locListRepository:    locListRepository,
 		subLocListRepository: subLocListRepository,
-		// visitLogRepository:   visitLogRepository,
-		logger: logger,
+		visitLogRepository:   visitLogRepository,
+		tbMapRepository:      tbMapRepository,
+		logger:               logger,
 	}
 }
 
@@ -41,92 +44,89 @@ func (s *LocationService) GetBadge(mm domain.MajorMinor, id int) (*domain.Locati
 		s.logger.Debug("[Service] GetBadge GetLocationByMM error")
 		return nil, fmt.Errorf("[Service] GetBadge GetLocationByMM error: %v", err)
 	}
-	s.logger.Sugar().Debugf("[Service] GetBadge %+v", locationId)
-	s.logger.Sugar().Debugf("[Service] GetBadge %+v", sublocationId)
-	// logger.Errorf("GetBadge %+v", locationId)
 
 	// GetTableByLocationId from tbMap to ensure which table to use
 
-	// resp.LocationName, err = s.locListRepository.GetNameByLocation(locationId)
-	// if err != nil {
-	// 	s.logger.Debug("[Service] GetBadge GetNameByLocation error")
-	// 	return nil, fmt.Errorf("[Service] GetBadge GetNameByLocation error: %v", err)
-	// }
+	resp.LocationName, err = s.locListRepository.GetNameByLocation(locationId)
+	if err != nil {
+		s.logger.Debug("[Service] GetBadge GetNameByLocation error")
+		return nil, fmt.Errorf("[Service] GetBadge GetNameByLocation error: %v", err)
+	}
 
-	// // record current sublocation to visit_log
-	// err = s.visitLogRepository.AddVisitLog(mm, id, locationId, sublocationId)
-	// if err != nil {
-	// 	s.logger.Debug("[Service] GetBadge AddVisitLog error")
-	// 	return nil, fmt.Errorf("[Service] GetBadge AddVisitLog error: %v", err)
-	// }
+	// record current sublocation to visit_log
+	err = s.visitLogRepository.AddVisitLog(mm, id, locationId, sublocationId)
+	if err != nil {
+		s.logger.Debug("[Service] GetBadge AddVisitLog error")
+		return nil, fmt.Errorf("[Service] GetBadge AddVisitLog error: %v", err)
+	}
 
-	// // get all sublocation info by locationId
-	// subBadgesFromDB, err := s.locationRepository.getSubLocListByLocId(locationId)
-	// if err != nil {
-	// 	s.logger.Debug("[Service] GetBadge getSubLocListByLocId error")
-	// 	return nil, fmt.Errorf("[Service] GetBadge getSubLocListByLocId error: %v", err)
-	// }
-	// // get user's visited record
-	// visitList, err := s.locationRepository.GetVisitedSubLocIdsByUserLocInfo(id, locationId)
-	// if err != nil {
-	// 	s.logger.Debug("[Service] GetBadge GetVisitedSubLocIdsByUserLocInfo error")
-	// 	return nil, fmt.Errorf("[Service] GetBadge GetVisitedSubLocIdsByUserLocInfo error: %v", err)
-	// }
-	// // compare sublocation info with user's visited record
-	// visitedMap := make(map[int]bool)
-	// for _, visit := range visitList {
-	// 	visitedMap[visit] = true
-	// }
+	// get all sublocation info by locationId
+	subBadgesFromDB, err := s.subLocListRepository.getSubLocListByLocId(locationId)
+	if err != nil {
+		s.logger.Debug("[Service] GetBadge getSubLocListByLocId error")
+		return nil, fmt.Errorf("[Service] GetBadge getSubLocListByLocId error: %v", err)
+	}
+	// get user's visited record
+	visitList, err := s.visitLogRepository.GetVisitedSubLocIdsByUserLocInfo(id, locationId)
+	if err != nil {
+		s.logger.Debug("[Service] GetBadge GetVisitedSubLocIdsByUserLocInfo error")
+		return nil, fmt.Errorf("[Service] GetBadge GetVisitedSubLocIdsByUserLocInfo error: %v", err)
+	}
+	// compare sublocation info with user's visited record
+	visitedMap := make(map[int]bool)
+	for _, visit := range visitList {
+		visitedMap[visit] = true
+	}
 
-	// var respSubBadges []domain.SubBadge
-	// countProgress := 0
-	// for _, subBadge := range subBadgesFromDB {
-	// 	badge := domain.Badge{
-	// 		IconPath:    subBadge.IconPath,
-	// 		Description: subBadge.Description,
-	// 	}
+	var respSubBadges []domain.SubBadge
+	countProgress := 0
+	for _, subBadge := range subBadgesFromDB {
+		badge := domain.Badge{
+			IconPath:    subBadge.IconPath,
+			Description: subBadge.Description,
+		}
 
-	// 	if visitedMap[subBadge.SubId] {
-	// 		badge.Aquired = true
-	// 		countProgress++
-	// 	} else {
-	// 		badge.Aquired = false
-	// 	}
+		if visitedMap[subBadge.SubId] {
+			badge.Aquired = true
+			countProgress++
+		} else {
+			badge.Aquired = false
+		}
 
-	// 	subBadgeTmp := domain.SubBadge{
-	// 		Badge: badge,
-	// 		SubId: subBadge.SubId,
-	// 	}
+		subBadgeTmp := domain.SubBadge{
+			Badge: badge,
+			SubId: subBadge.SubId,
+		}
 
-	// 	respSubBadges = append(respSubBadges, subBadgeTmp)
-	// }
+		respSubBadges = append(respSubBadges, subBadgeTmp)
+	}
 
-	// resp.SubBadge = respSubBadges
+	resp.SubBadge = respSubBadges
 
-	// resp.Progress = countProgress
-	// resp.TotalProgress, err = s.locationRepository.GetNumOfSubLocByLocId(locationId)
-	// if err != nil {
-	// 	s.logger.Debug("[Service] GetBadge GetNumOfSubLocByLocId error")
-	// 	return nil, fmt.Errorf("[Service] GetBadge GetNumOfSubLocByLocId error: %v", err)
-	// }
+	resp.Progress = countProgress
+	resp.TotalProgress, err = s.locationRepository.GetNumOfSubLocByLocId(locationId)
+	if err != nil {
+		s.logger.Debug("[Service] GetBadge GetNumOfSubLocByLocId error")
+		return nil, fmt.Errorf("[Service] GetBadge GetNumOfSubLocByLocId error: %v", err)
+	}
 
-	// // get main badge info by locationId
-	// mainBadgeDB, err := s.locationRepository.GetMainBadgeByLocationId(locationId)
-	// if err != nil {
-	// 	s.logger.Debug("[Service] GetBadge GetMainBadgeByLocationId error")
-	// 	return nil, fmt.Errorf("[Service] GetBadge GetMainBadgeByLocationId error: %v", err)
-	// }
+	// get main badge info by locationId
+	mainBadgeDB, err := s.locationRepository.GetMainBadgeByLocationId(locationId)
+	if err != nil {
+		s.logger.Debug("[Service] GetBadge GetMainBadgeByLocationId error")
+		return nil, fmt.Errorf("[Service] GetBadge GetMainBadgeByLocationId error: %v", err)
+	}
 
-	// mainBadge := domain.Badge{
-	// 	IconPath:    mainBadgeDB.IconPath,
-	// 	Description: mainBadgeDB.Description,
-	// }
-	// if resp.Progress == resp.TotalProgress {
-	// 	mainBadge.Aquired = true
-	// } else {
-	// 	mainBadge.Aquired = false
-	// }
-	// resp.MainBadge = mainBadge
+	mainBadge := domain.Badge{
+		IconPath:    mainBadgeDB.IconPath,
+		Description: mainBadgeDB.Description,
+	}
+	if resp.Progress == resp.TotalProgress {
+		mainBadge.Aquired = true
+	} else {
+		mainBadge.Aquired = false
+	}
+	resp.MainBadge = mainBadge
 
 	return &resp, nil
 }
