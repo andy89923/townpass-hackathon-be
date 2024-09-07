7766f1dd4c77eef3608f77d1e6099932e8126fa1
yccodr <taiwanmichael429@gmail.com>
2024-09-08 03:49:47 +0800

    chore: update json key name

--- pkg/domain/Location.go
+++ pkg/domain/Location.go
@@ -4,25 +4,25 @@ type MajorMinor uint32
 
 type Location struct {
 	MajorMinor MajorMinor // MajorMinor is a type for major and minor ，which are 4 bytes
-	UserId     int        `json:"user_id" form:"user_id"`
+	UserId     int        `json:"userId" form:"user_id"`
 	// LocationName string     `json:"name" form:"name"`
 	Name        string `json:"name" form:"name"`
 	Progress    int    `json:"progress" form:"progress"`
 	NumsOfSubId int    `json:"total" form:"total"`
 
-	MainBadge Badge      `json:"main_badge" form:"main_badge"`
-	SubBadge  []SubBadge `json:"sub_badges" form:"sub_badge"`
+	MainBadge Badge      `json:"mainBadge" form:"main_badge"`
+	SubBadge  []SubBadge `json:"subBadges" form:"sub_badge"`
 }
 
 type Badge struct {
-	IconPath    string `json:"icon_path" form:"icon_path"`
-	Aquired     bool   `json:"aquired" form:"aquired"`
+	IconPath    string            `json:"iconPath" form:"icon_path"`
+	Aquired     bool              `json:"aquired" form:"aquired"`
 	Description TempleDescription `json:"description" form:"description"`
 }
 
 type SubBadge struct {
 	Badge
-	SubId int `json:"sub_id" form:"sub_id"`
+	SubId          int `json:"subId" form:"sub_id"`
 	subDescription SubTempleDescription
 }
 
@@ -35,10 +35,9 @@ type TempleDescription struct {
 }
 
 type SubTempleDescription struct {
-	Deity string
+	Deity       string
 	Description string
 }
-
 
 type LocationRepository interface {
 	//TODO
