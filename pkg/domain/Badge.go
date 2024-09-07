444ad7582f03e43b890ef92d43bd79d73df19a79
HH <chh.cs12@nycu.edu.tw>
2024-09-07 06:00:29 +0000

    add domain badge

--- pkg/domain/Badge.go
+++ pkg/domain/Badge.go
@@ -1,5 +1,24 @@
 package domain
 
-type Badge struct {
+type MajorMinor struct {
+	Major int
+	Minor int
+}
 
+type Location_DB struct {
+	Location    int
+	name        string
+	Description string
+}
+
+type LocationSublocation_DB struct {
+	Location    int
+	Sublocation int
+	Description string
+}
+
+type VisitLog_DB struct {
+	UserId      int
+	Location    int
+	Sublocation int
 }
